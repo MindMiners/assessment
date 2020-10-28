16ce911c952c6e5a661ef925b100c196b32c90fb
Beckett Hanan <beckett.hanan@gmail.com>
2020-10-27 18:12:32 -0700

    Setting up api router and fixing body parser typo in server file. Adding details about schema in model file. Adding middleware to the post route in router file. Setting up controller with middleware to send user info to the database and to mailchimp if email consent is present.

--- server/model.js
+++ server/model.js
@@ -1,14 +1,27 @@
 const { Pool } = require('pg');
 const dotenv = require('dotenv');
+
 dotenv.config();
+
 // ELEPHANTSQL URI
 const URI = `postgres://${process.env.USERNAME}:${process.env.PASSWORD}@lallah.db.elephantsql.com:5432/${process.env.USERNAME}`;
+
 // create a pool with the database URI
 const pool = new Pool({ connectionString: URI });
 /* ** DB Schema is as follows:
-  ...
+  CREATE TABLE contacts (
+    id SERIAL PRIMARY KEY,
+    first_name character varying(50),
+    last_name character varying(50),
+    email character varying(320) NOT NULL UNIQUE,
+    type character varying(4),
+    email_consent boolean DEFAULT false,
+    took_assessment boolean DEFAULT false,
+    created_at timestamp without time zone DEFAULT now()
+  );
    ** And instructions for use are:
 */
+
 // Export an object with the query property as a function that logs the query
 // and then returns pool.query() with the same arguments passed in
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-const bodyParse = require('body-parser');
+const bodyParser = require('body-parser');
+const apiRouter = require('./router');
 
 const PORT = 3000;
 const app = express();
@@ -16,14 +17,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //   res.sendFile(path.join(__dirname, '../views/index.html'));
 // });
 
+// router to database
+app.use('/api/db', apiRouter);
+
 // send back a 404 error when we hit any routes we haven't defined
 app.get('*', (req, res) => {
   console.log('hit an unknown route');
   res.sendStatus(404);
 });
+
 // error handler for any errors we don't catch
 app.use((err) => {
   console.log('uncaught middleware error in express server ', err.message);
 });
+
 // start the server on the port we defined with a message to let us know it's running
 app.listen(PORT, () => console.log(`Listening on localhost:${PORT}`));
