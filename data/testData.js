59ec1b0df65803d938dbc4979494042404c7c67d
Veronica Stork <zealous.anemone@gmail.com>
2020-10-31 12:25:55 -0400

    Restructured data file to allow all objects to have access to data from other objects

--- data/testData.js
+++ data/testData.js
@@ -3,14 +3,14 @@ const testData = {};
 testData.NeTi = {
   name: 'NeTi',
   option: 'Option NeTi',
-  parent: testData.Ne,
+  // parent: testData.Ne,
   finalType: 'NeTi',
 };
 
 testData.NeFi = {
   name: 'NeFi',
   option: 'Option NeFi',
-  parent: testData.Ne,
+  // parent: testData.Ne,
   finalType: 'NeFi',
 };
 
@@ -19,14 +19,28 @@ testData.Ne = {
   question: 'text of the question 3',
   option: 'option Ne',
   answers: [testData.NeTi, testData.NeFi],
-  parent: testData.N,
+  // parent: testData.N,
+};
+
+testData.NiTe = {
+  name: 'NeTe',
+  option: 'Option NiTe',
+  // parent: testData.Ni,
+  finalType: 'NiTe',
+};
+
+testData.NiFe = {
+  name: 'NeFe',
+  option: 'Option NiFe',
+  // parent: testData.Ni,
+  finalType: 'NiFe',
 };
 testData.Ni = {
   name: 'Ni',
   question: 'text of the question 3',
   option: 'Option Ni',
   answers: [testData.NiTe, testData.NiFe],
-  parent: testData.N,
+  // parent: testData.N,
 };
 
 testData.N = {
@@ -34,21 +48,51 @@ testData.N = {
   question: 'text of the question 2',
   option: 'option N',
   answers: [testData.Ne, testData.Ni],
-  parent: testData.inputData,
+  // parent: testData.inputData,
 };
 
-testData.NiTe = {
-  name: 'NeTe',
-  option: 'Option NiTe',
-  parent: testData.Ni,
-  finalType: 'NiTe',
+testData.SeTi = {
+  name: 'SeTi',
+  option: 'Option SeTi',
+  // parent: testData.Se,
+  finalType: 'SeTi',
 };
 
-testData.NiFe = {
-  name: 'NeFe',
-  option: 'Option NiFe',
-  parent: testData.Ni,
-  finalType: 'NiFe',
+testData.SeFi = {
+  name: 'SeFi',
+  option: 'Option SeFi',
+  // parent: testData.Se,
+  finalType: 'SeFi',
+};
+
+testData.Se = {
+  name: 'Se',
+  question: 'text of the question 3',
+  option: 'option Se',
+  answers: [testData.SeTi, testData.SeFi],
+  // parent: testData.S,
+};
+
+testData.SiTe = {
+  name: 'SiTe',
+  option: 'Option SiTe',
+  // parent: testData.Si,
+  finalType: 'SiTe',
+};
+
+testData.SiFe = {
+  name: 'SiFe',
+  option: 'Option SiFe',
+  // parent: testData.Si,
+  finalType: 'SiFe',
+};
+
+testData.Si = {
+  name: 'Si',
+  question: 'text of the question 3',
+  option: 'Option Si',
+  answers: [testData.SiTe, testData.SiFe],
+  // parent: testData.S,
 };
 
 testData.S = {
@@ -58,48 +102,48 @@ testData.S = {
   answers: [testData.Se, testData.Si],
 };
 
-testData.Se = {
-  name: 'Se',
+testData.FeSi = {
+  name: 'FeSi',
+  option: 'Option FeSi',
+  // parent: testData.Fe,
+  finalType: 'FeSi',
+};
+
+testData.FeNi = {
+  name: 'FeNi',
+  option: 'Option FeNi',
+  // parent: testData.Fe,
+  finalType: 'FeNi',
+};
+
+testData.Fe = {
+  name: 'Fe',
   question: 'text of the question 3',
-  option: 'option Se',
-  answers: [testData.SeTi, testData.SeFi],
-  parent: testData.S,
+  option: 'option Fe',
+  answers: [testData.FeSi, testData.FeNi],
+  // parent: testData.F,
 };
 
-testData.SeTi = {
-  name: 'SeTi',
-  option: 'Option SeTi',
-  parent: testData.Se,
-  finalType: 'SeTi',
+testData.FiSe = {
+  name: 'FiSe',
+  option: 'Option FiSe',
+  // parent: testData.Fi,
+  finalType: 'FiSe',
 };
 
-testData.SeFi = {
-  name: 'SeFi',
-  option: 'Option SeFi',
-  parent: testData.Se,
-  finalType: 'SeFi',
+testData.FiNe = {
+  name: 'FiNe',
+  option: 'Option FiNe',
+  // parent: testData.Fi,
+  finalType: 'FiNe',
 };
 
-testData.Si = {
-  name: 'Si',
+testData.Fi = {
+  name: 'Fi',
   question: 'text of the question 3',
-  option: 'Option Si',
-  answers: [testData.SiTe, testData.SiFe],
-  parent: testData.S,
-};
-
-testData.SiTe = {
-  name: 'SiTe',
-  option: 'Option SiTe',
-  parent: testData.Si,
-  finalType: 'SiTe',
-};
-
-testData.SiFe = {
-  name: 'SiFe',
-  option: 'Option SiFe',
-  parent: testData.Si,
-  finalType: 'SiFe',
+  option: 'Option Fi',
+  answers: [testData.FiSe, testData.FiNe],
+  // parent: testData.F,
 };
 
 testData.F = {
@@ -109,55 +153,18 @@ testData.F = {
   answers: [testData.Fe, testData.Fi],
 };
 
-testData.Fe = {
-  name: 'Fe',
-  question: 'text of the question 3',
-  option: 'option Fe',
-  answers: [testData.FeSi, testData.FeNi],
-  parent: testData.F,
+testData.TeSi = {
+  name: 'TeSi',
+  option: 'Option TeSi',
+  // parent: testData.Te,
+  finalType: 'TeSi',
 };
 
-testData.FeSi = {
-  name: 'FeSi',
-  option: 'Option FeSi',
-  parent: testData.Fe,
-  finalType: 'FeSi',
-};
-
-testData.FeNi = {
-  name: 'FeNi',
-  option: 'Option FeNi',
-  parent: testData.Fe,
-  finalType: 'FeNi',
-};
-
-testData.Fi = {
-  name: 'Fi',
-  question: 'text of the question 3',
-  option: 'Option Fi',
-  answers: [testData.FiSe, testData.FiNe],
-  parent: testData.F,
-};
-
-testData.FiSe = {
-  name: 'FiSe',
-  option: 'Option FiSe',
-  parent: testData.Fi,
-  finalType: 'FiSe',
-};
-
-testData.FiNe = {
-  name: 'FiNe',
-  option: 'Option FiNe',
-  parent: testData.Fi,
-  finalType: 'FiNe',
-};
-
-testData.T = {
-  name: 'T',
-  option: 'Option T',
-  question: 'text of the question 2',
-  answers: [testData.Te, testData.Ti],
+testData.TeNi = {
+  name: 'TeNi',
+  option: 'Option TeNi',
+  // parent: testData.Te,
+  finalType: 'TeNi',
 };
 
 testData.Te = {
@@ -165,21 +172,21 @@ testData.Te = {
   question: 'text of the question 3',
   option: 'option Te',
   answers: [testData.TeSi, testData.TeNi],
-  parent: testData.T,
+  // parent: testData.T,
 };
 
-testData.TeSi = {
-  name: 'TeSi',
-  option: 'Option TeSi',
-  parent: testData.Te,
-  finalType: 'TeSi',
+testData.TiSe = {
+  name: 'TiSe',
+  option: 'Option TiSe',
+  // parent: testData.Ti,
+  finalType: 'TiSe',
 };
 
-testData.TeNi = {
-  name: 'TeNi',
-  option: 'Option TeNi',
-  parent: testData.Te,
-  finalType: 'TeNi',
+testData.TiNe = {
+  name: 'TiNe',
+  option: 'Option TiNe',
+  // parent: testData.Ti,
+  finalType: 'TiNe',
 };
 
 testData.Ti = {
@@ -187,21 +194,14 @@ testData.Ti = {
   question: 'text of the question 3',
   option: 'Option Ti',
   answers: [testData.TiSe, testData.TiNe],
-  parent: testData.T,
+  // parent: testData.T,
 };
 
-testData.TiSe = {
-  name: 'TiSe',
-  option: 'Option TiSe',
-  parent: testData.Ti,
-  finalType: 'TiSe',
-};
-
-testData.TiNe = {
-  name: 'TiNe',
-  option: 'Option TiNe',
-  parent: testData.Ti,
-  finalType: 'TiNe',
+testData.T = {
+  name: 'T',
+  option: 'Option T',
+  question: 'text of the question 2',
+  answers: [testData.Te, testData.Ti],
 };
 
 testData.inputData = {
