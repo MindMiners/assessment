0c75de77aba750b58f2aa272490b731d7d73a185
Beckett Hanan <beckett.hanan@gmail.com>
2020-10-31 14:18:04 -0700

    setting up dev server and regular server proxying

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,12 @@ module.exports = {
     path: __dirname + '/public',
     filename: 'build/app.js',
   },
+  devServer: { 
+    inline: true, 
+    contentBase: './dist', 
+    port: 3000, 
+    proxy: { "/api/**": { target: 'http://ec2-18-220-207-131.us-east-2.compute.amazonaws.com:3000', secure: false }  }
+  },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
   },
